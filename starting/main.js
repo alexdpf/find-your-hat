a83fd7dc63ebdf50bb46d7cc2a284c237a187682
Alex Fernandes <43734868+alexdpf@users.noreply.github.com>
2023-09-13 11:42:17 +1000

    add class Field and test

--- starting/main.js
+++ starting/main.js
@@ -1,6 +1,24 @@
+//IMPORTANT: prompt and other const already created. 
+//I had to install Nodemon globally for restating Node every time a change is made in the project, and prompt-sync to use it in Node
 const prompt = require('prompt-sync')({sigint: true});
 
 const hat = '^';
 const hole = 'O';
 const fieldCharacter = '░';
 const pathCharacter = '*';
+
+//2. create Field class
+class Field {
+    constructor(field) {
+        this._field = field;
+    }
+}
+//This variable was provided by Codecademy
+const myField = new Field([
+    ['*', '░', 'O'],
+    ['░', 'O', '░'],
+    ['░', '^', '░'],
+  ]);
+
+//2. TEST Field class constructor
+console.log(myField._field);
